82b5df6b7b1a2a8c3f233798cf2cf74b3500dd3d
Michael Waite <b1161976969@gmail.com>
2026-09-08 17:18:03 -0500

    style: load approved reference polish last

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.js';
 import './index.css';
 import './theme/npc-brain-replica.css';
 import './theme/npc-brain-runtime.css';
+import './theme/npc-approved-reference.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
