c74df37a818c1762be986a4981f4923c6aebc613
Michael Waite <b1161976969@gmail.com>
2026-09-08 04:00:56 -0500

    style: load cognitive runtime state layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import './index.css';
 import './theme/npc-brain-replica.css';
+import './theme/npc-brain-runtime.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
